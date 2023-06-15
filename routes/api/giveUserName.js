33087fbcaeebf205664bb0745e369c72a37dfe5c
HJUNG118 <a38722243@gmail.com>
2023-06-15 18:01:05 +0900

    Feat: userToken에서 userName추출하기 api 구현

---
+++ routes/api/giveUserName.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const { MongoClient } = require('mongodb');
+const conn_str = process.env.mongoURI;
+const jwt = require('jsonwebtoken');
+const { ObjectId } = require('mongodb');
+
+const extractUserName = async (token, secretKey) => {
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    const decodedUser = decoded.user; // 사용자 ID 반환
+    const userID = String(decodedUser.id);
+
+    const client = await MongoClient.connect(conn_str);
+    const database = client.db('test');
+    const usersCollection = database.collection('users');
+
+    const user = await usersCollection.findOne({ _id: new ObjectId(userID) });
+
+    if (user) {
+      const userName = user.name;
+      return userName;
+    } else {
+      throw new Error('User not found');
+    }
+  } catch (error) {
+    throw new Error('Invalid token');
+  }
+};
+
+router.get('/', async (req, res) => {
+  try {
+    const { userToken } = req.body;
+    // const authorizationHeader = req.headers.authorization;
+    // console.log(authorizationHeader)
+    // if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+    //   const userToken = authorizationHeader.substring(7); // "Bearer " 부분을 제외한 토큰 값 추출
+    //   console.log(userToken);
+    // }
+    const username = await extractUserName(userToken, process.env.jwtSecret);
+    res.json({ username });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json('이름을 추출하지 못했습니다.');
+  }
+});
+
+module.exports = router;

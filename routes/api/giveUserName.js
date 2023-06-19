2f8a420e56084557f4f41707324af28720662419
이상운 <sangunlee6@gmail.com>
2023-06-20 03:02:44 +0900

    chore: register.js 주석 제거

--- routes/api/giveUserName.js
+++ routes/api/giveUserName.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const conn_str = process.env.mongoURI;
-const jwt = require('jsonwebtoken');
-const { ObjectId } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const { ObjectId } = require("mongodb");
 
 const extractUserName = async (token, secretKey) => {
   try {
@@ -12,8 +12,8 @@ const extractUserName = async (token, secretKey) => {
     const userID = String(decodedUser.id);
 
     const client = await MongoClient.connect(conn_str);
-    const database = client.db('test');
-    const usersCollection = database.collection('users');
+    const database = client.db("test");
+    const usersCollection = database.collection("users");
 
     const user = await usersCollection.findOne({ _id: new ObjectId(userID) });
 
@@ -21,26 +21,26 @@ const extractUserName = async (token, secretKey) => {
       const userName = user.name;
       return userName;
     } else {
-      throw new Error('User not found');
+      throw new Error("User not found");
     }
   } catch (error) {
-    throw new Error('Invalid token');
+    throw new Error("Invalid token");
   }
 };
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     // const { userToken } = req.body;
     const authorizationHeader = req.headers.authorization;
     let userToken = null;
-    if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+    if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
       userToken = authorizationHeader.substring(7); // "Bearer " 부분을 제외한 토큰 값 추출
     }
     const username = await extractUserName(userToken, process.env.jwtSecret);
     res.json({ username });
   } catch (error) {
     console.log(error);
-    res.status(500).json('이름을 추출하지 못했습니다.');
+    res.status(500).json("이름을 추출하지 못했습니다.");
   }
 });
 

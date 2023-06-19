9d4788da71a0f0a9ac2419418d9a01bac5586c3e
이상운 <sangunlee6@gmail.com>
2023-06-20 03:11:59 +0900

    feat: 새로운 DB에 맞춰 deleteUserScrap, deleteKeyWord 수정

--- routes/api/deleteUserScrap.js
+++ routes/api/deleteUserScrap.js
@@ -29,55 +29,11 @@ const extractUserName = async (token, secretKey) => {
   }
 };
 
-// 스크랩 하나 삭제
-const deleteUserScrap = async (username, url, title, date, res) => {
-  try {
-    const client = await MongoClient.connect(conn_str);
-    const database = client.db('search');
-    const userScrapCollection = database.collection(username);
-    const result = await userScrapCollection.findOne({ user: username });
-    let dataDeleted = false;
-
-    // keyWords 배열을 순회하면서 데이터 삭제
-    for (let i = 0; i < result.keyWords.length; i++) {
-      const keywordData = result.keyWords[i].data;
-      const keywordDate = result.keyWords[i].date;
-      if (keywordDate !== date) {
-        continue;
-      } else {
-        const dataIndex = keywordData.findIndex((item) => item.title === title && item.url === url);
-        if (dataIndex !== -1) {
-          keywordData.splice(dataIndex, 1);
-          dataDeleted = true;
-
-          if (keywordData.length === 0) {
-            // 데이터 배열이 비었을 경우 해당 키워드 삭제
-            result.keyWords.splice(i, 1);
-            if (result.keyWords.length === 0) {
-              // keyWords 배열이 모두 비었을 경우 컬렉션 삭제
-              await userScrapCollection.drop();
-            }
-          }
-          break; // 삭제 완료 후 루프 종료
-        }
-      }
-    }
-    if (!dataDeleted) {
-      // 해당 데이터가 없을 경우 에러 응답
-      res.status(404);
-      return { message: '해당 데이터를 찾을 수 없음' };
-    }
-    await userScrapCollection.updateOne({ user: username }, { $set: { keyWords: result.keyWords } });
-    return { message: '데이터 삭제 완료' };
-  } catch (error) {
-    console.error(error);
-    res.status(500).json('스크랩 삭제 오류');
-  }
-};
+const { deleteUserScrap } = require("../../function/deleteUserScrap");
 
 router.delete('/', async (req, res) => {
+  const { url, title, date } = req.body;
   try {
-    const { url, title, date } = req.body;
     const authorizationHeader = req.headers.authorization;
     let userToken = null;
     if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {

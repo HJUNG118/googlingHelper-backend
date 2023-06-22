c33c449e21c4ea42b052d4138314711e6aab224e
HJUNG118 <a38722243@gmail.com>
2023-06-22 16:02:35 +0900

    chore: 없음

--- server.js
+++ server.js
@@ -1,54 +1,53 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+app.get('/', (req, res) => {
+  res.send('API Running');
 });
 
-const authRouter = require("./routes/api/auth");
-app.use("/api/auth", authRouter);
+const authRouter = require('./routes/api/auth');
+app.use('/api/auth', authRouter);
 
-const registerRouter = require("./routes/api/register");
-app.use("/api/register", registerRouter);
+const registerRouter = require('./routes/api/register');
+app.use('/api/register', registerRouter);
 
-const loginRouter = require("./routes/api/login");
-app.use("/api/login", loginRouter);
+const loginRouter = require('./routes/api/login');
+app.use('/api/login', loginRouter);
 
-const logoutRouter = require("./routes/api/logout");
-app.use("/api/logout", logoutRouter);
+const logoutRouter = require('./routes/api/logout');
+app.use('/api/logout', logoutRouter);
 
-const deleteUserScrapRouter = require("./routes/api/deleteUserScrap");
-app.use("/api/deleteUserScrap", deleteUserScrapRouter);
+const deleteUserScrapRouter = require('./routes/api/deleteUserScrap');
+app.use('/api/deleteUserScrap', deleteUserScrapRouter);
 
-const deleteKeyWordRouter = require("./routes/api/deleteKeyWord");
-app.use("/api/deleteKeyWord", deleteKeyWordRouter);
+const deleteKeyWordRouter = require('./routes/api/deleteKeyWord');
+app.use('/api/deleteKeyWord', deleteKeyWordRouter);
 
-const saveUserScrapRouter = require("./routes/api/saveUserScrap");
-app.use("/api/saveUserScrap", saveUserScrapRouter);
+const saveUserScrapRouter = require('./routes/api/saveUserScrap');
+app.use('/api/saveUserScrap', saveUserScrapRouter);
 
-const keyWordByDateRouter = require("./routes/api/keyWordByDate");
-app.use("/api/keyWordByDate", keyWordByDateRouter);
+const keyWordByDateRouter = require('./routes/api/keyWordByDate');
+app.use('/api/keyWordByDate', keyWordByDateRouter);
 
-const giveUserName = require("./routes/api/giveUserName");
-app.use("/api/giveUserName", giveUserName);
+const giveUserName = require('./routes/api/giveUserName');
+app.use('/api/giveUserName', giveUserName);
 
-const checkEmail = require("./routes/api/checkEmail");
-app.use("/api/checkEmail", checkEmail);
+const checkEmail = require('./routes/api/checkEmail');
+app.use('/api/checkEmail', checkEmail);
 
-const saveScrapTextRouter = require("./routes/api/saveScrapText");
-app.use("/api/saveScrapText", saveScrapTextRouter);
+const saveScrapTextRouter = require('./routes/api/saveScrapText');
+app.use('/api/saveScrapText', saveScrapTextRouter);
 
-const deleteScrapTextRouter = require("./routes/api/deleteScrapText");
-app.use("/api/deleteScrapText", deleteScrapTextRouter);
-
+const deleteScrapTextRouter = require('./routes/api/deleteScrapText');
+app.use('/api/deleteScrapText', deleteScrapTextRouter);
 
 connectDB();
 

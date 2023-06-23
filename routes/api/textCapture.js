f83b4a1e4e72b00e33ef63f804c075fb18dafc26
SungBeum <an4126@gmail.com>
2023-06-24 00:30:04 +0900

    chore: chunk 받기 완료

--- routes/api/textCapture.js
+++ routes/api/textCapture.js
@@ -15,10 +15,10 @@ const MY_OCR_SECRET_KEY = process.env.MY_OCR_SECRET_KEY;
 const { getDateAndTime } = require('../../function/getDateAndTime');
 const { extractUserName } = require('../../function/extractUserName');
 const { saveScrap } = require('../../function/saveScrap');
-
+let accumulatedTexts = '';
 router.post('/', async (req, res) => {
   try {
-    const { keyWord, title, texts } = req.body;
+    const { keyWord, title, textChunk, isLastChunk } = req.body; // 'isLastChunk' 플래그를 추가합니다.
     const authorizationHeader = req.headers.authorization;
     let userToken = null;
     if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
@@ -26,13 +26,24 @@ router.post('/', async (req, res) => {
     }
     const dateTime = await getDateAndTime();
     const username = await extractUserName(userToken, process.env.jwtSecret);
-    const result = await processImageAsync(username, keyWord, dateTime.date, dateTime.time, title, texts);
-    res.status(200).json({ message: result });
+
+    accumulatedTexts += textChunk; // 받은 텍스트 청크를 누적합니다.
+
+    // 마지막 청크가 받아졌을 때 누적된 텍스트를 처리합니다.
+    if (isLastChunk) { // 마지막 청크인지 판단하는 방법을 구현해야 합니다.
+      const result = await processImageAsync(username, keyWord, dateTime.date, dateTime.time, title, accumulatedTexts);
+      res.status(200).json({ message: result });
+      accumulatedTexts = ''; // 누적된 텍스트를 리셋합니다.
+    } else {
+      res.status(200).json({ message: 'Text chunk received successfully.' });
+    }
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
   }
 });
+
 
 // Asynchronous function to process the image
 const processImageAsync = async (username, keyWord, date, time, title, texts) => {
@@ -45,7 +56,7 @@ const processImageAsync = async (username, keyWord, date, time, title, texts) =>
     },
   };
   const timestamp = new Date().getTime();
-  const combinedText = chunks.join('');
+  const combinedText = texts.join('');
   try {
     console.log(combinedText);
     // const response = await axios.post(

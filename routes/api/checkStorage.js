2e28ee3d785a7f90cf31e518aa90738d243e1e84
HJUNG118 <a38722243@gmail.com>
2023-06-28 01:22:52 +0900

    Feat: checkStorage username 돌려주기

--- routes/api/checkStorage.js
+++ routes/api/checkStorage.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const { extractUserName } = require("../../function/extractUserName");
-const { checkStorage } = require("../../function/checkStorage");
+const { extractUserName } = require('../../function/extractUserName');
+const { checkStorage } = require('../../function/checkStorage');
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const authorizationHeader = req.headers.authorization;
     let userToken = null;
-    if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
+    if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
       userToken = authorizationHeader.substring(7);
     }
 
@@ -16,13 +16,13 @@ router.post("/", async (req, res) => {
     const dataToSend = await checkStorage(username);
 
     if (dataToSend.length === 0) {
-      res.status(200).json({ message: "데이터가 없습니다." });
+      res.status(200).json(username);
     } else {
       res.status(200).json({ dataToSend, username });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Error-checkStorage" });
+    res.status(500).json({ message: 'Error-checkStorage' });
   }
 });
 

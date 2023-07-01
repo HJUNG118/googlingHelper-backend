0b5feb058f320dfc64d61ce05521b39e2951d511
이상운 <sangunlee6@gmail.com>
2023-07-01 15:34:23 +0900

    fix: deleteMemo db 수정

--- function/deleteMemo.js
+++ function/deleteMemo.js
@@ -1,24 +1,20 @@
-require('dotenv').config();
-const { client } = require('../config/mongodb');
-
+require("dotenv").config();
+const { connectDB, getDB } = require("../config/mongodb");
 
 // 스크랩 텍스트 삭제
 const deleteMemo = async (username, time) => {
   try {
-    const database = client.db('memo');
-    const userScrapCollection = database.collection(username);
+    await connectDB("memo");
+    const userScrapCollection = getDB("memo").collection(username);
     const deletionResult = await userScrapCollection.deleteOne({
       time: time,
     });
     if (deletionResult.deletedCount === 0) {
-      client.close();
-      return { message: 'Nonexistent' };
+      return { message: "Nonexistent" };
     }
-    client.close();
-    return { message: 'success' };
+    return { message: "success" };
   } catch (error) {
-    client.close();
-    return { message: 'error' };
+    return { message: "error" };
   }
 };
 

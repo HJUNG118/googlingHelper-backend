c6d4d459318cb35ee2fda5b0f308e9875e367f04
이상운 <sangunlee6@gmail.com>
2023-07-01 13:33:07 +0900

    fix: register db 수정

--- routes/api/register.js
+++ routes/api/register.js
@@ -2,21 +2,25 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const router = express.Router();
 require('dotenv').config();
-const { client } = require('../../config/mongodb');
+const { connectDB, getDB } = require('../../config/mongodb');
+
+
+
+
+
 
 router.post("/", async (req, res) => {
   const { name, email, password } = req.body;
-
+  
   try {
-
-    const db = client.db("test");
-
-    const existingEmail = await db.collection("users").findOne({ email });
+    await connectDB('test');
+    
+    const existingEmail = await getDB('test').collection("users").findOne({ email });
     if (existingEmail) {
       return res.status(400).json({ errors: [{ msg: "User already exists" }] });
     }
 
-    const existingName = await db.collection("users").findOne({ name });
+    const existingName = await getDB('test').collection("users").findOne({ name });
     if (existingName) {
       return res.status(400).json({ errors: [{ msg: "User already exists" }] });
     }
@@ -24,7 +28,7 @@ router.post("/", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    await db.collection("users").insertOne({
+    await getDB('test').collection("users").insertOne({
       name,
       email,
       password: hashedPassword,
@@ -34,8 +38,6 @@ router.post("/", async (req, res) => {
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
-  } finally {
-    await client.close(); // MongoDB 연결 종료
   }
 });
 

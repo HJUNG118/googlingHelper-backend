d6ec9fc0a31a261f0876961218cd8045c02cc513
이상운 <sangunlee6@gmail.com>
2023-07-01 14:49:20 +0900

    fix: saveScrap db 수정

--- function/saveScrap.js
+++ function/saveScrap.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
-const { client } = require('../config/mongodb');
+const { ObjectId } = require('mongodb');
+const { connectDB, getDB } = require('../config/mongodb');
+
 
 const saveScrap = async (username, keyWord, url, date, time, title, texts, img) => {
   try {
-    const session = client.startSession(); // 세션 생성
-    session.startTransaction(); // 트랜잭션 시작
-    const database = client.db('scrapData');
-    const scrapCollection = database.collection(username);
+    await connectDB('scrapData');    
+    // const session = getDB('scrapData').startSession(); // 세션 생성
+    // session.startTransaction(); // 트랜잭션 시작
+
+    // console.log(username);
+    // console.log(keyWord);
+    // console.log(url);
+    // console.log(date);
+    // console.log(time);
+    // console.log(title);
+    // console.log(texts);
+    // console.log(img);
+    
+    const scrapCollection = getDB('scrapData').collection(username);
     const existingScrap = await scrapCollection.findOne({ date: date, title: title });
     let updateResult;
     if (existingScrap) {
@@ -16,11 +28,9 @@ const saveScrap = async (username, keyWord, url, date, time, title, texts, img) 
       } else if (img && img.length > 0) {
         updateResult = await scrapCollection.updateOne({ _id: existingScrapId }, { $push: { img: img } });
       } else {
-        client.close();
         return 'duplicate';
       }
       if (updateResult && updateResult.modifiedCount > 0) {
-        client.close();
         return 'update';
       } else {
         throw new Error('Failed to update');
@@ -39,16 +49,15 @@ const saveScrap = async (username, keyWord, url, date, time, title, texts, img) 
 
       const insertResult = await scrapCollection.insertOne(newScrap);
       if (insertResult.insertedId) {
-        client.close();
         return 'complete';
       } else {
         throw new Error('Failed to insert');
       }
     }
   } catch (error) {
-    client.close();
     return Promise.reject(error);
   }
 };
 
 module.exports = { saveScrap };
+

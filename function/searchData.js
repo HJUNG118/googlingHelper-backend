133f1e6247e8d68d56422ace836cfa6043611418
HJUNG118 <a38722243@gmail.com>
2023-07-02 01:33:41 +0900

    Feat: 문자열 패턴 검색 가능

--- function/searchData.js
+++ function/searchData.js
@@ -1,35 +1,32 @@
-require("dotenv").config();
-const { connectDB, getDB } = require("../config/mongodb");
+require('dotenv').config();
+const { connectDB, getDB } = require('../config/mongodb');
 
 const searchData = async (username, search) => {
   try {
     // const session = client.startSession();
     // session.startTransaction();
 
-    await connectDB("scrapData");
-    const scrapCollection = getDB("scrapData").collection(username);
+    await connectDB('scrapData');
+    const scrapCollection = getDB('scrapData').collection(username);
 
     // Check if the 'text' index already exists
     const indexes = await scrapCollection.indexes();
-    const textIndexExists = indexes.some((index) => index.name === "text_1");
+    const textIndexExists = indexes.some((index) => index.name === 'text_1');
 
     // If the 'text' index doesn't exist, create it
     if (!textIndexExists) {
-      await scrapCollection.createIndex({ text: "text" });
+      await scrapCollection.createIndex({ text: 'text' });
     }
 
     // Using text search for 'search' term
-    const documents = await scrapCollection
-      .find({ $text: { $search: search } })
-      .toArray();
+    const documents = await scrapCollection.find({ text: { $regex: search } }).toArray();
 
     let result = [];
     for (const doc of documents) {
-      const foundElements = doc.text.filter((element) =>
-        element.includes(search)
-      );
+      const foundElements = doc.text.filter((element) => new RegExp(search, 'i').test(element));
       result = result.concat(foundElements);
     }
+    console.log(result);
 
     return result;
   } catch (error) {

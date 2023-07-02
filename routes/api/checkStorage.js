cd2128507769d8e2e652d2c228c31cd383f9245f
이상운 <sangunlee6@gmail.com>
2023-07-02 17:14:52 +0900

    feat: brotli 적용

--- routes/api/checkStorage.js
+++ routes/api/checkStorage.js
@@ -1,28 +1,81 @@
-const express = require('express');
+// const express = require('express');
+// const router = express.Router();
+
+// const { extractUserName } = require('../../function/extractUserName');
+// const { checkStorage } = require('../../function/checkStorage');
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const authorizationHeader = req.headers.authorization;
+//     let userToken = null;
+//     if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+//       userToken = authorizationHeader.substring(7);
+//     }
+
+//     const username = await extractUserName(userToken);
+//     const dataToSend = await checkStorage(username);
+
+//     if (dataToSend.length === 0) {
+//       res.status(200).json(username);
+//     } else {
+//       res.status(200).json({ dataToSend, username });
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Error-checkStorage' });
+//   }
+// });
+
+// module.exports = router;
+
+/* 아래는 brotli 적용 */
+
+const express = require("express");
 const router = express.Router();
+const zlib = require("zlib");
+const util = require("util");
+const brotliSettings = {
+  params: {
+    [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+    [zlib.constants.BROTLI_PARAM_QUALITY]: 2, // Default is 11, lower it to reduce CPU load.
+  },
+};
+const brotliCompress = util.promisify(zlib.brotliCompress);
+const { extractUserName } = require("../../function/extractUserName");
+const { checkStorage } = require("../../function/checkStorage");
 
-const { extractUserName } = require('../../function/extractUserName');
-const { checkStorage } = require('../../function/checkStorage');
-
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const authorizationHeader = req.headers.authorization;
     let userToken = null;
-    if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+    if (authorizationHeader && authorizationHeader.startsWith("Bearer ")) {
       userToken = authorizationHeader.substring(7);
     }
 
     const username = await extractUserName(userToken);
     const dataToSend = await checkStorage(username);
 
+    let responsePayload;
+
     if (dataToSend.length === 0) {
-      res.status(200).json(username);
+      responsePayload = Buffer.from(JSON.stringify(username));
     } else {
-      res.status(200).json({ dataToSend, username });
+      responsePayload = Buffer.from(JSON.stringify({ dataToSend, username }));
     }
+
+    if (
+      req.headers["accept-encoding"] &&
+      req.headers["accept-encoding"].includes("br")
+    ) {
+      res.setHeader("Content-Encoding", "br");
+      res.setHeader("Content-Type", "application/json");
+      responsePayload = await brotliCompress(responsePayload, brotliSettings);
+    }
+
+    res.status(200).send(responsePayload);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error-checkStorage' });
+    res.status(500).json({ message: "Error-checkStorage" });
   }
 });
 

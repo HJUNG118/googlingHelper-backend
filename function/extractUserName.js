41563d202f4ae6f0f01b18dfc4f72c96aef2bc4e
이상운 <sangunlee6@gmail.com>
2023-07-02 18:32:48 +0900

    feat: memory-cache 적용

--- function/extractUserName.js
+++ function/extractUserName.js
@@ -1,38 +1,44 @@
-const { ObjectId } = require('mongodb');
-const jwt = require('jsonwebtoken');
+const { ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const cache = require("memory-cache");
 const secretKey = process.env.jwtSecret;
-require('dotenv').config();
-const { connectDB, getDB } = require('../config/mongodb');
+require("dotenv").config();
+const { connectDB, getDB } = require("../config/mongodb");
 
-const { isTokenBlacklisted } = require('../middleware/tokenBlacklist');
+const { isTokenBlacklisted } = require("../middleware/tokenBlacklist");
 
 const extractUserName = async (token) => {
-
   try {
-    await connectDB('test'); 
+    await connectDB("test");
 
     const TokenBlacklisted = isTokenBlacklisted(token);
     if (TokenBlacklisted) {
-      throw new Error('Token is blacklisted');
+      throw new Error("Token is blacklisted");
     }
 
     if (token === undefined) {
-      console.log('Token is undefined');
-      throw new Error('Token is undefined');
+      console.log("Token is undefined");
+      throw new Error("Token is undefined");
+    }
+
+    let userName = cache.get(token);
+    if (userName) {
+      return userName;
     }
 
     const decoded = jwt.verify(token, secretKey);
     const decodedUser = decoded.user; // 사용자 ID 반환
     const userID = String(decodedUser.id);
-    const database = getDB('test')
-    const usersCollection = database.collection('users');
+    const database = getDB("test");
+    const usersCollection = database.collection("users");
     const user = await usersCollection.findOne({ _id: new ObjectId(userID) });
 
     if (user) {
-      const userName = user.name;
+      userName = user.name;
+      cache.put(token, userName, 3600 * 1000);
       return userName;
     } else {
-      return { message: 'User not found' };
+      return { message: "User not found" };
     }
   } catch (error) {
     return Promise.reject(error);

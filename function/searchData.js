b1936c49eacc111e13d06015020c39b4194245b7
HJUNG118 <a38722243@gmail.com>
2023-07-03 19:22:11 +0900

    feat: collection이 존재하지 않을 때 예외처리

--- function/searchData.js
+++ function/searchData.js
@@ -1,33 +1,30 @@
-require("dotenv").config();
-const { connectDB, getDB } = require("../config/mongodb");
+require('dotenv').config();
+const { connectDB, getDB } = require('../config/mongodb');
 
 const searchData = async (username, search) => {
   try {
-    // const session = client.startSession();
-    // session.startTransaction();
+    await connectDB('scrapData');
+    const db = getDB('scrapData');
+    let result = [];
+    const collections = await db.listCollections({ name: username }).toArray();
+    const scrapCollectionExists = collections.length > 0;
 
-    await connectDB("scrapData");
-    const scrapCollection = getDB("scrapData").collection(username);
-
-    // Check if the 'text' index already exists
-    const indexes = await scrapCollection.indexes();
-    const textIndexExists = indexes.some((index) => index.name === "text_1");
-
-    // If the 'text' index doesn't exist, create it
-    if (!textIndexExists) {
-      await scrapCollection.createIndex({ text: "text" });
+    if (!scrapCollectionExists) {
+      return result; // 빈 배열 반환
     }
 
-    // Using text search for 'search' term
-    const documents = await scrapCollection
-      .find({ text: { $regex: search } })
-      .toArray();
+    const scrapCollection = db.collection(username);
 
-    let result = [];
+    const indexes = await scrapCollection.indexes();
+    const textIndexExists = indexes.some((index) => index.name === 'text_1');
+
+    if (!textIndexExists) {
+      await scrapCollection.createIndex({ text: 'text' });
+    }
+    const documents = await scrapCollection.find({ text: { $regex: search } }).toArray();
+
     for (const doc of documents) {
-      const foundElements = doc.text.filter((element) =>
-        new RegExp(search, "i").test(element)
-      );
+      const foundElements = doc.text.filter((element) => new RegExp(search, 'i').test(element));
       result = result.concat(foundElements);
     }
 

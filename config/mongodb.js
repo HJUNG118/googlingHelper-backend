44f379c6b7dbb21b0ab72be7a8309d462466a809
이상운 <sangunlee6@gmail.com>
2023-07-04 21:11:36 +0900

    feat: DB 연결 로직 변경, 이미 연결된 client라면 connectDB()를 하지 않도록 변경

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,10 +4,18 @@ const { MongoClient } = require('mongodb');
 const conn_str = process.env.mongoURI;
 const client = new MongoClient(conn_str, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const connectedDBs = {}; // 이미 연결된 클라이언트를 추적하기 위한 객체
+
 const connectDB = async (dbName = 'default') => {
   try {
+    if (connectedDBs[dbName]) {
+      console.log(`Already connected to ${dbName}`);
+      return;
+    }
+
     await client.connect();
-    console.log('MongoDB Connected');
+    connectedDBs[dbName] = true;
+    console.log(`MongoDB Connected to ${dbName}`);
   } catch (err) {
     console.error('MongoDB Connection Error:', err);
     process.exit(1);
@@ -18,7 +26,7 @@ const getDB = (dbName = 'default') => {
   if (!client.topology.isConnected()) {
     throw Error('DB not connected. Call connectDB first.');
   }
-  return client.db(dbName);  // dbName 인자로 원하는 DB를 선택
+  return client.db(dbName);
 };
 
 module.exports = { connectDB, getDB };
